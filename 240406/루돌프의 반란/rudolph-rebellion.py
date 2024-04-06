e08242781b9e6ca08cbbfb390efedd3f196d3a36
ssum1ra <101113206+ssum1ra@users.noreply.github.com>
2024-04-06 22:00:59 +0900

    Title: 루돌프의 반란, Time: 264ms, Memory: 26MB, Status: Passed - CodeTree

--- 240406/루돌프의 반란/rudolph-rebellion.py
+++ 240406/루돌프의 반란/rudolph-rebellion.py
@@ -114,10 +114,9 @@ for t in range(1, m+1):
                 moveDir = dir
 
         if moveDir != -1:
-            nx = pos[i][0] + dx[moveDir]
-            ny = pos[i][1] + dy[moveDir]
-
-            # 산타의 이동으로 충돌한 경우, 산타를 이동시키고 처리를 합니다.
+            nx = pos[i][0]+dx[moveDir]
+            ny = pos[i][1]+dy[moveDir]
+            # 산타의 이동으로 루돌프와 충돌한 경우
             if nx == rudolf[0] and ny == rudolf[1]:
                 stun[i] = t + 1
 
@@ -129,8 +128,7 @@ for t in range(1, m+1):
                 lastX, lastY = firstX, firstY
 
                 if d == 1:
-                    points[i] += d
-
+                    points[i] += 1
                 else:
                     while is_inrange(lastX, lastY) and board[lastX][lastY] > 0:
                         lastX += moveX
